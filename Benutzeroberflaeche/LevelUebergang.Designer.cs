10b52bf4e1cdc715e9321ec2647b14cb827ccb25
dlatikaynen <dlatikay@outlook.com>
2021-04-04 20:41:54 +0200

    [dlatikay 20210404] GUI für die Levelübergänge, Teil 1 von #26

---
+++ Benutzeroberflaeche/LevelUebergang.Designer.cs
@@ -0,0 +1,230 @@
+﻿
+namespace ScorchGore.Benutzeroberflaeche
+{
+    partial class LevelUebergang
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Ueberschrift = new System.Windows.Forms.Label();
+            this.StatischAktuell = new System.Windows.Forms.Label();
+            this.AktuellesLevel = new System.Windows.Forms.Label();
+            this.AktuellesLevelDetails = new System.Windows.Forms.Label();
+            this.StatischBevorstehend = new System.Windows.Forms.Label();
+            this.StatischErgebnisse = new System.Windows.Forms.Label();
+            this.ErgebnisBesiegt = new System.Windows.Forms.Label();
+            this.ErgebnisGewonnen = new System.Windows.Forms.Label();
+            this.MissionAbbrechenKnopf = new System.Windows.Forms.Button();
+            this.MissionFortsetzenKnopf = new System.Windows.Forms.Button();
+            this.KommendesLevelDetails = new System.Windows.Forms.Label();
+            this.KommendesLevel = new System.Windows.Forms.Label();
+            this.NoiceBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // Ueberschrift
+            // 
+            this.Ueberschrift.AutoSize = true;
+            this.Ueberschrift.Location = new System.Drawing.Point(12, 9);
+            this.Ueberschrift.Name = "Ueberschrift";
+            this.Ueberschrift.Size = new System.Drawing.Size(251, 13);
+            this.Ueberschrift.TabIndex = 0;
+            this.Ueberschrift.Text = "{0} and {1} on a Wrath of the Mild series of missions";
+            this.Ueberschrift.UseMnemonic = false;
+            // 
+            // StatischAktuell
+            // 
+            this.StatischAktuell.AutoSize = true;
+            this.StatischAktuell.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StatischAktuell.Location = new System.Drawing.Point(12, 26);
+            this.StatischAktuell.Name = "StatischAktuell";
+            this.StatischAktuell.Size = new System.Drawing.Size(75, 13);
+            this.StatischAktuell.TabIndex = 1;
+            this.StatischAktuell.Text = "Currently on";
+            this.StatischAktuell.UseMnemonic = false;
+            // 
+            // AktuellesLevel
+            // 
+            this.AktuellesLevel.AutoSize = true;
+            this.AktuellesLevel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AktuellesLevel.Location = new System.Drawing.Point(12, 43);
+            this.AktuellesLevel.Name = "AktuellesLevel";
+            this.AktuellesLevel.Size = new System.Drawing.Size(96, 13);
+            this.AktuellesLevel.TabIndex = 2;
+            this.AktuellesLevel.Text = "Level {0}: \"{1}\"";
+            this.AktuellesLevel.UseMnemonic = false;
+            // 
+            // AktuellesLevelDetails
+            // 
+            this.AktuellesLevelDetails.AutoSize = true;
+            this.AktuellesLevelDetails.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AktuellesLevelDetails.Location = new System.Drawing.Point(12, 60);
+            this.AktuellesLevelDetails.Name = "AktuellesLevelDetails";
+            this.AktuellesLevelDetails.Size = new System.Drawing.Size(187, 13);
+            this.AktuellesLevelDetails.TabIndex = 3;
+            this.AktuellesLevelDetails.Text = "Being the {0} level in the \"{1}\" mission";
+            this.AktuellesLevelDetails.UseMnemonic = false;
+            // 
+            // StatischBevorstehend
+            // 
+            this.StatischBevorstehend.AutoSize = true;
+            this.StatischBevorstehend.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StatischBevorstehend.Location = new System.Drawing.Point(12, 171);
+            this.StatischBevorstehend.Name = "StatischBevorstehend";
+            this.StatischBevorstehend.Size = new System.Drawing.Size(66, 13);
+            this.StatischBevorstehend.TabIndex = 4;
+            this.StatischBevorstehend.Text = "Coming up";
+            this.StatischBevorstehend.UseMnemonic = false;
+            // 
+            // StatischErgebnisse
+            // 
+            this.StatischErgebnisse.AutoSize = true;
+            this.StatischErgebnisse.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StatischErgebnisse.Location = new System.Drawing.Point(12, 89);
+            this.StatischErgebnisse.Name = "StatischErgebnisse";
+            this.StatischErgebnisse.Size = new System.Drawing.Size(49, 13);
+            this.StatischErgebnisse.TabIndex = 5;
+            this.StatischErgebnisse.Text = "Results";
+            this.StatischErgebnisse.UseMnemonic = false;
+            // 
+            // ErgebnisBesiegt
+            // 
+            this.ErgebnisBesiegt.AutoSize = true;
+            this.ErgebnisBesiegt.Location = new System.Drawing.Point(12, 124);
+            this.ErgebnisBesiegt.Name = "ErgebnisBesiegt";
+            this.ErgebnisBesiegt.Size = new System.Drawing.Size(109, 13);
+            this.ErgebnisBesiegt.TabIndex = 7;
+            this.ErgebnisBesiegt.Text = "{0} noobed out by {1}";
+            this.ErgebnisBesiegt.UseMnemonic = false;
+            // 
+            // ErgebnisGewonnen
+            // 
+            this.ErgebnisGewonnen.AutoSize = true;
+            this.ErgebnisGewonnen.Location = new System.Drawing.Point(12, 106);
+            this.ErgebnisGewonnen.Name = "ErgebnisGewonnen";
+            this.ErgebnisGewonnen.Size = new System.Drawing.Size(99, 13);
+            this.ErgebnisGewonnen.TabIndex = 6;
+            this.ErgebnisGewonnen.Text = "{0} won, {1} HP left";
+            this.ErgebnisGewonnen.UseMnemonic = false;
+            // 
+            // MissionAbbrechenKnopf
+            // 
+            this.MissionAbbrechenKnopf.DialogResult = System.Windows.Forms.DialogResult.Abort;
+            this.MissionAbbrechenKnopf.Location = new System.Drawing.Point(111, 237);
+            this.MissionAbbrechenKnopf.Name = "MissionAbbrechenKnopf";
+            this.MissionAbbrechenKnopf.Size = new System.Drawing.Size(93, 23);
+            this.MissionAbbrechenKnopf.TabIndex = 9;
+            this.MissionAbbrechenKnopf.Text = "Abort mission";
+            this.MissionAbbrechenKnopf.UseVisualStyleBackColor = true;
+            // 
+            // MissionFortsetzenKnopf
+            // 
+            this.MissionFortsetzenKnopf.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.MissionFortsetzenKnopf.Location = new System.Drawing.Point(12, 237);
+            this.MissionFortsetzenKnopf.Name = "MissionFortsetzenKnopf";
+            this.MissionFortsetzenKnopf.Size = new System.Drawing.Size(93, 23);
+            this.MissionFortsetzenKnopf.TabIndex = 8;
+            this.MissionFortsetzenKnopf.Text = "Let\'s go!";
+            this.MissionFortsetzenKnopf.UseVisualStyleBackColor = true;
+            // 
+            // KommendesLevelDetails
+            // 
+            this.KommendesLevelDetails.AutoSize = true;
+            this.KommendesLevelDetails.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KommendesLevelDetails.Location = new System.Drawing.Point(12, 205);
+            this.KommendesLevelDetails.Name = "KommendesLevelDetails";
+            this.KommendesLevelDetails.Size = new System.Drawing.Size(187, 13);
+            this.KommendesLevelDetails.TabIndex = 11;
+            this.KommendesLevelDetails.Text = "Being the {0} level in the \"{1}\" mission";
+            this.KommendesLevelDetails.UseMnemonic = false;
+            // 
+            // KommendesLevel
+            // 
+            this.KommendesLevel.AutoSize = true;
+            this.KommendesLevel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KommendesLevel.Location = new System.Drawing.Point(12, 188);
+            this.KommendesLevel.Name = "KommendesLevel";
+            this.KommendesLevel.Size = new System.Drawing.Size(96, 13);
+            this.KommendesLevel.TabIndex = 10;
+            this.KommendesLevel.Text = "Level {0}: \"{1}\"";
+            this.KommendesLevel.UseMnemonic = false;
+            // 
+            // NoiceBox
+            // 
+            this.NoiceBox.AutoSize = true;
+            this.NoiceBox.Location = new System.Drawing.Point(387, 237);
+            this.NoiceBox.Name = "NoiceBox";
+            this.NoiceBox.Size = new System.Drawing.Size(55, 17);
+            this.NoiceBox.TabIndex = 12;
+            this.NoiceBox.Text = "noice!";
+            this.NoiceBox.UseVisualStyleBackColor = true;
+            // 
+            // LevelUebergang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 272);
+            this.Controls.Add(this.NoiceBox);
+            this.Controls.Add(this.KommendesLevelDetails);
+            this.Controls.Add(this.KommendesLevel);
+            this.Controls.Add(this.MissionAbbrechenKnopf);
+            this.Controls.Add(this.MissionFortsetzenKnopf);
+            this.Controls.Add(this.ErgebnisBesiegt);
+            this.Controls.Add(this.ErgebnisGewonnen);
+            this.Controls.Add(this.StatischErgebnisse);
+            this.Controls.Add(this.StatischBevorstehend);
+            this.Controls.Add(this.AktuellesLevelDetails);
+            this.Controls.Add(this.AktuellesLevel);
+            this.Controls.Add(this.StatischAktuell);
+            this.Controls.Add(this.Ueberschrift);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LevelUebergang";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Mission Report";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Ueberschrift;
+        private System.Windows.Forms.Label StatischAktuell;
+        private System.Windows.Forms.Label AktuellesLevel;
+        private System.Windows.Forms.Label AktuellesLevelDetails;
+        private System.Windows.Forms.Label StatischBevorstehend;
+        private System.Windows.Forms.Label StatischErgebnisse;
+        private System.Windows.Forms.Label ErgebnisBesiegt;
+        private System.Windows.Forms.Label ErgebnisGewonnen;
+        private System.Windows.Forms.Button MissionAbbrechenKnopf;
+        private System.Windows.Forms.Button MissionFortsetzenKnopf;
+        private System.Windows.Forms.Label KommendesLevelDetails;
+        private System.Windows.Forms.Label KommendesLevel;
+        private System.Windows.Forms.CheckBox NoiceBox;
+    }
+}
